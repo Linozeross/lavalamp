48993ab68324f15c03a18c590db370e61d7156d2
Silas Oettinghaus <sioe@tf.uni-kiel.de>
2023-11-04 17:23:52 +0100

    keyboard lib hinzugefügt

--- __main__.py
+++ __main__.py
@@ -5,17 +5,19 @@ import numpy as np
 import mido 
 import camFeatureExtractor as cfe
 import midiGenerator as mg
+import keyboard
 
 print("Welcome to the lavalamp 💡!")
 
 
-print(mido.get_output_names())
+
+
+#print(mido.get_output_names())
 #create a new output stream
-outport = mido.open_output('IAC-Treiber lavalamp')
+outport = mido.open_output(mido.get_output_names()[0])
 
 
 cap = cv2.VideoCapture(0)
-
 while(True):
     ret, frame = cap.read()
 
@@ -24,11 +26,9 @@ while(True):
     print("Brightness: {}".format(brightness))
     mg.outputNote(outport, brightness)
 
-    cv2.imshow('frame', frame)
-
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #exit this while loop when q is pressed
+    if keyboard.is_pressed('q'):
         break
-
 
 
 #close the output stream
