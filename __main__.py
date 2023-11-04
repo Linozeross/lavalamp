af1147466b0e340671e59c83593e6364a45fe59d
Lino Toran Jenner <lino@dayoff.de>
2023-11-04 22:06:29 +0100

    clamp and accord

--- __main__.py
+++ __main__.py
@@ -5,34 +5,107 @@ import numpy as np
 import mido 
 import camFeatureExtractor as cfe
 import midiGenerator as mg
-import keyboard
+import time
+
+BPM = 60
 
 print("Welcome to the lavalamp 💡!")
 
-
-
-
 #print(mido.get_output_names())
 #create a new output stream
-outport = mido.open_output(mido.get_output_names()[0])
+
+outports = []
+
+for port in mido.get_output_names():
+    print(port)
+    outports.append(mido.open_output(port))
+
+
+sleepTime = 60.0/BPM
+
+
 
 
 cap = cv2.VideoCapture(0)
+
+#change resolution to 320x240
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+
+
+#mg.startNoteOutput(outport)
+
+
+# get current timestamp
+lastTimeStamp = time.time()
+print(lastTimeStamp)
+
+lastNotes = None
+lastBaseNote = None
+
+minBrightness = None
+maxBrightness = None
+
+counter = 0
+
+
+
+
+
+
 while(True):
-    ret, frame = cap.read()
 
-    # Extract the brightness
-    brightness = cfe.extractNormalizedBrightness(frame)
-    print("Brightness: {}".format(brightness))
-    mg.outputNote(outport, brightness)
+    #if it's been long enough since the last note, play a new one
+    
+    timeJump = time.time() - lastTimeStamp
+    if timeJump > sleepTime:
+        
+        counter += 1
+        counter = counter % 2
+        print(counter)
 
-    #exit this while loop when q is pressed
-    if keyboard.is_pressed('q'):
-        break
+        ret, frame = cap.read()
+        # Extract the brightness
+        brightness = cfe.extractNormalizedBrightness(frame)
+
+        if minBrightness is None or brightness < minBrightness:
+            minBrightness = brightness
+    
+        if maxBrightness is None or brightness > maxBrightness:
+            maxBrightness = brightness
+
+        print("Brightness: {}; Min: {}; Max: {}".format(brightness, minBrightness, maxBrightness))
+
+
+        if lastNotes is not None:
+            mg.stopNotes(outports[0], lastNotes)
+        
+        lastNotes = mg.outputNote(outports[0], brightness)
+        
+
+
+        # #Base on 1
+        # if counter == 0:
+        #     if lastBaseNote is not None:
+        #         mg.stopNote(outports[0], lastBaseNote)
+
+        #     lastBaseNote = mg.playBase(outports[0])
+        #     #mg.startNoteOutput(outport)
+        
+        
+        
+        lastTimeStamp = time.time()
+        print(round((time.time() - lastTimeStamp) - sleepTime), 4)
+
+   
+    #mg.outputNote(outport, brightness)
+
+    #mg.pitchNote(outport, brightness)
 
 
 #close the output stream
-outport.close()
+for outport in outports:
+    outport.close()
 cap.release()
 cv2.destroyAllWindows()
 
--- midiGenerator.py
+++ midiGenerator.py
@@ -1,22 +1,73 @@
 import mido
 import time
 
+def clampBrightness(brightness):
+    #clamp brightness to 0.2 - 0.8
+    if brightness < 0.2:
+        brightness = 0.2
+    elif brightness > 0.8:
+        brightness = 0.8
+    
+    # scale to 0 - 1
+    brightness = (brightness - 0.2) / 0.6
+    return brightness
+
+
 
 #plays a note for 1 second depending on a number between 0 and 1; 
 # 0 is low, 1 is high (in a given range) and outputs to stream
 def outputNote(stream, input):
 
-    note = int(100*input)
-    print(note)
+    input = clampBrightness(input)
+
+    #convert input to a note
+    note = 30 + round(input * 50)
+
+    grTerz = note + 3
+    klTerz = note + 5
+
     #create a note on message
-    on = mido.Message('note_on', note=note)
-    #create a note off message
-    off = mido.Message('note_off', note=note)
+    on = mido.Message('note_on', note=note, channel=0, velocity=63)
+    on2 = mido.Message('note_on', note=grTerz, channel=0, velocity=63)
+    on3 = mido.Message('note_on', note=klTerz, channel=0, velocity=63)
     #send the note on message
     stream.send(on)
-    #wait for the given amount of time
-    time.sleep(0.1)
-    #send the note off message
-    stream.send(off)
-    #close the output stream
+    stream.send(on2)
+    stream.send(on3)
+
+    print(note)
+
+    return [note, grTerz, klTerz]
   
+def startNoteOutput(stream):
+    #create a note on message
+    on = mido.Message('note_on', note=60, velocity=127)
+    #send the note on message
+    stream.send(on)
+
+def pitchNote(stream, input):
+    pitch = int(16000*input - 8000)
+    note = 60
+    #create a pitchwheel message
+    pitch = mido.Message('pitchwheel', pitch=pitch)
+    #send the pitchwheel message
+    stream.send(pitch)
+
+   
+def stopNotes(stream, notes):
+    for note in notes:
+        #create a note off message
+        off = mido.Message('note_off', note=note, velocity=63)
+        #send the note off message
+        stream.send(off)
+
+
+def playBase(stream):
+    #create a note on message
+    note = 39
+
+    on = mido.Message('note_on', note = note, velocity=127, time=0.5, channel=1)
+    #send the note on message
+    stream.send(on)
+    return note
+   
